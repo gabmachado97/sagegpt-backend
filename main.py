c2d267c22d950175983ed1ccb122e473b6a9331e
Gabriel Machado <61938035+gabmachado97@users.noreply.github.com>
2024-12-07 18:29:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask('App')
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def welcome():
-  return "<h1>asdfasdf</h1>"
+@app.route('/api/value', methods=['GET'])
+def get_value():
+    return jsonify({"value": "Hello from Flask!"})
 
-if __name__ == 'main':
-  app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
