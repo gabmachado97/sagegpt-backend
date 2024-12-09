607bf1b9d5c8693bc82b4fd4e8aa496b6a583746
Gabriel Machado <61938035+gabmachado97@users.noreply.github.com>
2024-12-09 16:32:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Response, make_response
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain.vectorstores import Qdrant
@@ -18,6 +18,7 @@ def _build_cors_preflight_response():
     response.headers.add("Access-Control-Allow-Methods", "*")
     return response
 
+
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
@@ -33,11 +34,12 @@ vs = Qdrant.from_existing_collection(
 
 chat_model = ChatGroq(
     temperature=0,
-    model_name="llama3-8b-8192",
+    model_name="gemma-7b-it",
+    #model_name="llama-3.1-70b-versatile",
     api_key="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",
 )
 
-retriever = vs.as_retriever(search_kwargs={"k": 1, "fetch_k": 10})
+retriever = vs.as_retriever(search_kwargs={"k": 1})
 
 custom_prompt_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -49,6 +51,7 @@ Only return the helpful answer below and nothing else.
 Helpful answer:
 """
 
+
 def set_custom_prompt():
     """
     Prompt template for QA retrieval for each vectorstore
@@ -56,38 +59,38 @@ def set_custom_prompt():
     prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
     return prompt
 
+
 prompt = set_custom_prompt()
 
 qa = RetrievalQA.from_chain_type(
     llm=chat_model,
     chain_type="stuff",
     retriever=retriever,
-    return_source_documents=False,
+    return_source_documents=True,
     chain_type_kwargs={"prompt": prompt},
 )
 
+
 @app.route("/api/value", methods=["POST", "OPTIONS"])
 def handle_query():
-    if request.method == "OPTIONS":  # CORS preflight
-        return _build_cors_preflight_response()
-    elif request.method == "POST":  # Receive query
-        data = request.get_json()  # Get JSON data
-        query = data.get("query", "")
-        if query:
-            try:
-                # Stream the response
-                def stream_response():
-                    response = qa.run(query)  # Run the query with RetrievalQA
-                    # Simulate chunking (you can customize this logic)
-                    for i in range(0, len(response), 50):  # Chunk size = 50 characters
-                        yield f"data: {response[i:i+50]}\n\n"
-                    yield "event: end\ndata: \n\n"  # Send an 'end' event
 
-                return Response(stream_response(), content_type="text/event-stream")
-            except Exception as e:
-                return jsonify({"error": str(e)}), 500
-        else:
-            return jsonify({"error": "No query provided"}), 400
+    if request.method == "OPTIONS": # CORS preflight
+      return _build_cors_preflight_response()
+    else:
+      data = request.get_json()  # Receive JSON data from React
+  
+      query = data.get("query", "")
+
+      if query:
+          try:
+              response = qa.invoke({"query": query})
+              
+              result = response["result"]  # Extract the result from the response
+              return _corsify_actual_response(jsonify({"result": result}))  # Send back the result as JSON
+          except Exception as e:
+              return _corsify_actual_response(jsonify({"error": str(e)}), 500 )
+      else:
+          return _corsify_actual_response(jsonify({"error": "No query provided"}), 400)
 
 
 if __name__ == "__main__":
